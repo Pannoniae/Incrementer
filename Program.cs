19a2e4f28b40aa439a9e7b7f4033e294120e67cf
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-04-12 01:42:33 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ulong i = 0;
+while (true) {
+    if (i % 1_000_000_000 == 0) {
+        Console.Out.WriteLine(i);
+    }
+    i++;
+}

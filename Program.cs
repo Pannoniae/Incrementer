ce15125448c2cef9c6ef3df935ffa59d11cc19f5
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2024-05-10 18:35:05 +0100

    - different experiment (the MSDN docs are wrong btw)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿ulong i = 0;
-while (true) {
-    if (i % 1_000_000_000 == 0) {
-        Console.Out.WriteLine(i);
+﻿public struct IntHolder {
+    public int i;
+}
+
+
+public class Program {
+    public static void Main(string[] args) {
+
+        IntHolder y = new IntHolder();
+        y.i = 5;
+        Foo(ref y);
+        Console.WriteLine(y.i);
+        Foo2(ref y);
+        Console.WriteLine(y.i);
     }
-    i++;
+
+    public static void Foo(ref IntHolder x) {
+        x.i = 10;
+    }
+
+    public static void Foo2(ref IntHolder x) {
+        x = new IntHolder();
+    }
 }
